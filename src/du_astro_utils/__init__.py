fbbc706712961b7ad9b8944bd4abf79a72d7c58a
Joseph Chevalier <joe_ontheflex@hotmail.fr>
2024-02-24 11:51:02 +0100

    First build as a pip-installable python package. Archives location determined by the environment variable.

--- src/du_astro_utils/__init__.py
+++ src/du_astro_utils/__init__.py
@@ -1,3 +1,25 @@
-from .example_module import greetings, meaning
+# pylint : skip-file
 
-__all__ = ["greetings", "meaning"]
+# from .example_module import greetings, meaning
+from .calibration import *
+from .photometry import *
+from .utils import *
+
+__all__ = [
+    "get_calib_dirs_photometry",
+    "get_infos_from_image",
+    "check_obs_night",
+    "load_bias_frames",
+    "load_dark_frames",
+    "load_flat_frames",
+    "master_bias",
+    "master_dark",
+    "master_flat",
+    "reduce_sci_image",
+    "gaussian",
+    "snr",
+    "detect_sources",
+    "get_fwhm",
+    "apert_photometry",
+    "query_sso_photometry",
+]
